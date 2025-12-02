cc6434e47c9fe70e45fc29cf148b543251a450be
Gajapathi K <113296010+ViratGaja@users.noreply.github.com>
2025-12-02 11:57:24 +0530

    changes

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import {assets} from '../assets/assets'
 const Login = () => {
   const [state,setState]= useState('sign up')
   return (
-    <div className='flex items-center justify-center min-h-screen'>
+    <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
       <img src={assets.logo} className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' alt="" />
       <div>
         <h2>{state==="sign up"?"Create account":"Login to your account!"}</h2>
